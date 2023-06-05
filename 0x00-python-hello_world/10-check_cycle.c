d032325859a92f5f20e9f1e609d3237ccb33c840
BradleyGilden <bradleygilden@gmail.com>
2023-06-05 20:33:57 +0200

    Program to check cycle in linked list

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * check_cycle - detects for cycles in linked lists
+ * @list: pointer to head node in list
+ *
+ * Return: 0 if no cycle, 1 otherwise
+ * Please note other resource files that work this this file can be found...
+ * ... in the resources folder
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *fast = NULL;
+	listint_t *slow = NULL;
+
+	if (list == NULL)
+		return (0);
+
+	slow = list;
+	fast = list->next;
+
+	while (fast->next != NULL && slow != NULL)
+	{
+		if (slow == fast)
+			return (1);
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+	return (0);
+}
