339478b53f83a4d5d75e65931fa0c3239a13921b
BradleyGilden <bradleygilden@gmail.com>
2023-06-06 13:07:06 +0200

    longer comment test

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-for i in range(0, 100):  # comment
+for i in range(0, 100):  # comment lsdifjskdfjsld slkdfjsdkjfslslkdfj s
     if i != 99:
         print("{:02d}".format(i), end=", ")
     else:
