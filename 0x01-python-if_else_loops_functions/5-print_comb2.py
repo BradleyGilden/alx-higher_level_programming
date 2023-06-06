7d397a89b3335594a913db5c415d64f0257efe37
BradleyGilden <bradleygilden@gmail.com>
2023-06-06 13:05:13 +0200

    no comment

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
-for i in range(0, 100):
-# prints numbers in decimal seperated by a comma    
+for i in range(0, 100):   
     if i != 99:
         print("{:02d}".format(i), end=", ")
     else:
