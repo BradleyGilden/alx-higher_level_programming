e9e8a4bc39c4c2eeb703374b29c848eafd34b6e2
BradleyGilden <bradleygilden@gmail.com>
2023-06-06 12:45:36 +0200

    print int seperated by comma

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+
+for i in range(0, 100):  # prints numbers in decimal seperated by comma
+    if i != 99:
+        print("{0:02d},".format(i), end=" ")
+    else:
+        print("{0:02d}".format(i))
