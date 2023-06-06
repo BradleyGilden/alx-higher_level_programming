7ede1c6fdc428377c3c45dd8bb70471ba8e2900f
BradleyGilden <bradleygilden@gmail.com>
2023-06-06 14:22:43 +0200

    fixed conditional

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,10 +2,11 @@
 def uppercase(str):  # converts string to uppercase
     endchar = ""
     i = 1
-    for c in str:
-        if i == len(str):
-            endchar = "\n"
-        if ord(c) >= 97 and ord(c) <= 122:
-            c = chr(ord(c) - 32)
-        print("{}".format(c), end=endchar)
-        i += 1
+    if str != "":
+        for c in str:
+            if i == len(str):
+                endchar = "\n"
+            if ord(c) >= 97 and ord(c) <= 122:
+                c = chr(ord(c) - 32)
+            print("{}".format(c), end=endchar)
+            i += 1
