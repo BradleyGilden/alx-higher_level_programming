4e90cfbc1716bd5d913d09d7935e14fe2fa9fa19
BradleyGilden <bradleygilden@gmail.com>
2023-06-06 12:16:18 +0200

    format change

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
-for i in range(97, 123):  # prints alphabet using ASCII
+for i in range(97, 123):  # prints lower alphabet using ASCII
     print("{0:c}".format(i), end="")
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,1 +1,5 @@
 #!/usr/bin/python3
+
+for i in range(97, 123):  # prints lower ASCII alphabet except e and q
+    if i != 101 and i != 113:
+        print("{0:c}".format(i), end="")
