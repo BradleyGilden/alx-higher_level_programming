1d3477ec431a66995139843c121bd4dc5278a2fe
BradleyGilden <bradleygilden@gmail.com>
2023-06-06 13:08:46 +0200

    comment fix

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-for i in range(0, 100):  # comment lsdifjskdfjsld slkdfjsdkjfslslkdfj s
+for i in range(0, 100):  # displays a number combination seprated by ", "
     if i != 99:
         print("{:02d}".format(i), end=", ")
     else:
