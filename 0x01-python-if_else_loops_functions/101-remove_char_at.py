ef44831c9f23cc60c24a2a59c6b87c7c669aea7a
BradleyGilden <bradleygilden@gmail.com>
2023-06-06 19:44:36 +0200

    remove char at index

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# removes char at index
+def remove_char_at(str, n):
+    if str is None:
+        return None;
+    if n > len(str) or n < 0:
+        return str
+
+    str = str[:n] + str[n + 1:]
+    return str
