8a61cbe3200df5afda20defac435ba46911a29fd
BradleyGilden <bradleygilden@gmail.com>
2023-06-06 13:02:17 +0200

    comment

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for number in range(0, 100): # print combination seperated by a comma ,
+for number in range(0, 100):  # print string seperated by a comma,space
     if number == 99:
         print("{}".format(number))
     else:
