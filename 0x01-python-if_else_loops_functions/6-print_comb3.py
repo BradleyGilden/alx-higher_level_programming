42fc6e262277f90c840ca23f99967e07bc863d98
BradleyGilden <bradleygilden@gmail.com>
2023-06-06 13:20:47 +0200

    printing more combinations

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+# display numbers from 00 to 99
+# numbers should  not be repeated like 00, 11, 22
+# sequences should not be repeated like 10 and 01
+for i in range(0, 10):
+    for j in range(0, 10):
+        if (j > i):
+            if (i == 8 and j == 9):
+                print("{0:d}{1:d}".format(i, j))
+                exit(0)
+            else:
+                print("{0:d}{1:d}".format(i, j), end=", ")
