071fc45e1c1f1b8afe8b592c5163cd18c45a1282
BradleyGilden <bradleygilden@gmail.com>
2023-06-06 11:04:15 +0200

    check sign

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+import random
+
+
+number = random.randint(-10, 10)
+
+if number > 0:  # condition checks the sign of number returned from randint()
+    print(number, "is positive")
+elif number < 0:
+    print(number, "is negative")
+else:
+    print(number, "is zero")
