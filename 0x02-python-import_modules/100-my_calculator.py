342bd321241aadda4d0df83059554d7f36ba1988
BradleyGilden <bradleygilden@gmail.com>
2023-06-08 14:51:23 +0200

    improved calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+from sys import argv
+
+
+# perfecting calculator program with error messages from 1_calculator
+# if arg_count != 3, print Usage: ./100-my_calculator.py <a> <operator> <b>
+# if operator is unknown, print:
+# Unknown operator. Available operators: +, -, * and /
+def my_switch(sign, a, b):
+    if sign == '+':
+        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    elif sign == '-':
+        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    elif sign == '*':
+        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    elif sign == '/':
+        print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+
+
+if __name__ == '__main__':
+
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    else:
+        a = int(argv[1])
+        sign = argv[2]
+        b = int(argv[3])
+        my_switch(sign, a, b)
