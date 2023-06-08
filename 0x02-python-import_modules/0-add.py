10c9ddeffee3a756f1fdf4a09ef823dec549ff09
BradleyGilden <bradleygilden@gmail.com>
2023-06-08 12:08:40 +0200

    removed comments

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -5,5 +5,4 @@ from add_0 import add
 if __name__ == '__main__':
     a = 1
     b = 2
-    # imports method "add" from file "add_0" to be run in py script
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
