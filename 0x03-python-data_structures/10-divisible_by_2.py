7eb8adc4ad765fe3e8d8ca4f079da9028f796bc4
BradleyGilden <bradleygilden@gmail.com>
2023-06-09 18:57:14 +0200

    divisible by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+
+# return a bool list of True/False depending if a number is divisible by 2
+
+def divisible_by_2(my_list=[]):
+    bool_list = []
+
+    for num in my_list:
+        if num % 2 == 0:
+            bool_list.append(True)
+        else:
+            bool_list.append(False)
+
+    return(bool_list)
