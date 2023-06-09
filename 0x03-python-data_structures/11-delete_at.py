4dc804c247e673fe5db3eaf264e0e9bbecb0fcf3
BradleyGilden <bradleygilden@gmail.com>
2023-06-09 19:59:17 +0200

    delete item from list

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+
+# removes item from list without list methods
+def delete_at(my_list=[], idx=0):
+    l_len = len(my_list) - 1
+
+    if idx < 0 or idx > l_len:
+        return my_list
+
+    del my_list[idx]
+
+    return my_list
