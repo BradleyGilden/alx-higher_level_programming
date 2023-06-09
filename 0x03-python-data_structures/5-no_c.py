901a0f38b9d2ab253f828a4a0470031b5a9a355c
BradleyGilden <bradleygilden@gmail.com>
2023-06-09 11:17:33 +0200

    remove C from string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+
+# removes all occurences of C and c in string
+def no_c(my_string):
+
+    my_list = list(my_string)
+
+    for i in range(len(my_list)):
+        if my_list[i] == 'C' or my_list[i] == 'c':
+            my_list[i] = ''
+
+    return("".join(my_list))
