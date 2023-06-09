d77e3f86ca7107bd10401e4eb7252f6b8abdd80a
BradleyGilden <bradleygilden@gmail.com>
2023-06-09 10:33:23 +0200

    return element at index

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+
+# a function that retrieves an element from a list like in C
+def element_at(my_list, idx):
+    a_len = len(my_list)
+
+    if idx > a_len - 1 or idx < 0:
+        return None
+
+    return my_list[idx]
