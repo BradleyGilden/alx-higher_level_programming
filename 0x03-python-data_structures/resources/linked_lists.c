79759ce0bd9f991e9836d5bc19347b27b095e306
BradleyGilden <bradleygilden@gmail.com>
2023-06-10 17:23:26 +0200

    testing script

--- 0x03-python-data_structures/resources/linked_lists.c
+++ 0x03-python-data_structures/resources/linked_lists.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all elements of a listint_t list
+ * print_listint - prints all elements of listint_t list
  * @h: pointer to head of list
  * Return: number of nodes
  */
