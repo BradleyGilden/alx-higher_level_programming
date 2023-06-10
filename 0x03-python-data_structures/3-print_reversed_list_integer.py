02cb6f7be40eba434ae59f6587770fd9016cc759
Bradley Gilden <bradleygilden@gmail.com>
2023-06-10 08:47:58 +0200

    checked for instance of list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,5 +3,8 @@
 # prints reversed int list without casting
 def print_reversed_list_integer(my_list=[]):
 
-    for num in my_list[::-1]:
-        print("{:d}".format(num))
+    if isinstance(my_list, list):
+        my_list.reversed()
+
+        for num in my_list:
+            print("{:d}".format(num))
