174ecaeb2ac148323a4f83b8b35903bb6727060f
BradleyGilden <bradleygilden@gmail.com>
2023-06-14 18:09:43 +0200

    multiply copied dict by 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+
+# multiplies values my 2 and returns a new dict
+def multiply_by_2(a_dictionary):
+    new_dict = a_dictionary.copy()
+    for key in new_dict.keys():
+        new_dict[key] = new_dict[key] * 2
+
+    return new_dict
