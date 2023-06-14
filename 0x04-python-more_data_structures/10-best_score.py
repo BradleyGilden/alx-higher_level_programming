9ccc78bf133aa9a4e8ce753990dd6fcf0b6acf66
BradleyGilden <bradleygilden@gmail.com>
2023-06-14 18:34:13 +0200

    removed return

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -6,8 +6,6 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
 
-    return max(a_dictionary)
-
     large_name = a_dictionary.keys()[0]
     large_value = a_dictionary[large_name]
 
