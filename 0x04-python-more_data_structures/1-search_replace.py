fccf1dece1034b2bc2c0cab0482a4e01a2abe86c
BradleyGilden <bradleygilden@gmail.com>
2023-06-14 16:36:52 +0200

    search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+
+# for every 'search' value, replace it with variabe: replace
+def search_replace(my_list, search, replace):
+    if my_list == [] or search not in my_list:
+        return my_list
+    new_list = my_list[:]
+
+    for i in range(0, len(new_list)):
+        new_list[i] = replace if new_list[i] == search else new_list[i]
+
+    return new_list
