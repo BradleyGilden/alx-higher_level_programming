fd53352a35e5fa61085cdf837069461220ab903b
BradleyGilden <bradleygilden@gmail.com>
2023-06-14 18:57:34 +0200

    checks length

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@
 # returns key with biggest score
 def best_score(a_dictionary):
 
-    if a_dictionary is None or not isinstance(a_dictionary, dict):
+    if a_dictionary is None or len(a_dictionary) == 0:
         return None
 
     large_name = list(a_dictionary.keys())[0]
