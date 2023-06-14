7566da30524b4d4a96ccb01f134cdfff0ed71bd0
BradleyGilden <bradleygilden@gmail.com>
2023-06-14 17:30:34 +0200

    print sorted dictionary

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+
+# return a sorted dictionary based on key strings
+def print_sorted_dictionary(a_dictionary):
+    for key, value in sorted(a_dictionary.items()):
+        print(f"{key}: {value}")
