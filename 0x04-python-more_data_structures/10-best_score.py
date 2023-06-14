7fe9973caaf930d0713fa1bca3a3ccca1eadf4ce
BradleyGilden <bradleygilden@gmail.com>
2023-06-14 18:56:41 +0200

    checked instance

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@
 # returns key with biggest score
 def best_score(a_dictionary):
 
-    if a_dictionary is None:
+    if a_dictionary is None or not isinstance(a_dictionary, dict):
         return None
 
     large_name = list(a_dictionary.keys())[0]
