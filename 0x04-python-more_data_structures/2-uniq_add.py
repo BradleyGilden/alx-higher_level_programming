0ae50256a6b9df5d2cb4fa7d7b6fd54158721f7d
BradleyGilden <bradleygilden@gmail.com>
2023-06-14 16:46:46 +0200

    removed return list

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
+# add unique elements in list
 def uniq_add(my_list=[]):
     sum = 0
-
-    if my_list == []:
-        return my_list
 
     for num in set(my_list):
         sum += num
