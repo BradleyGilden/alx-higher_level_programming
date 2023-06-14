66e75c252fd0c80bbf8104017c20710d8783e8cb
BradleyGilden <bradleygilden@gmail.com>
2023-06-14 16:21:11 +0200

    return square of matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    if matrix == []:
+        return matrix
+    new_matrix = []
+    for i in range(0, len(matrix)):
+        new_matrix.append(list(map(lambda x: x ** 2, matrix[i])))
+
+    return (new_matrix)
