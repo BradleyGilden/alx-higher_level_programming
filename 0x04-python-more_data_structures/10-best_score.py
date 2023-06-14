46e553c2d2d534504971abbb6424fd63efe7f990
BradleyGilden <bradleygilden@gmail.com>
2023-06-14 18:33:14 +0200

    return largest value in dictionary

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,17 @@
 # returns key with biggest score
 def best_score(a_dictionary):
 
-    if a_dictionary is None or len(a_dictionary) == 0:
+    if a_dictionary is None:
         return None
 
     return max(a_dictionary)
+
+    large_name = a_dictionary.keys()[0]
+    large_value = a_dictionary[large_name]
+
+    for key, value in a_dictionary.items():
+        if value > large_value:
+            large_value = value
+            large_name = key
+
+    return large_name
