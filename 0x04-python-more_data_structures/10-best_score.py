e0812f51e2e1a3a6bb0f40b9b7b1e09951727c5c
BradleyGilden <bradleygilden@gmail.com>
2023-06-14 18:24:47 +0200

    check for dict len

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@
 # returns key with biggest score
 def best_score(a_dictionary):
 
-    if a_dictionary is None:
+    if a_dictionary is None or len(a_dictionary) == 0:
         return None
 
     return max(a_dictionary)
