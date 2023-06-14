6a2fc0a8cda6198c1848859d6b72ef6b4bb9ef91
BradleyGilden <bradleygilden@gmail.com>
2023-06-14 16:42:07 +0200

    add unique elements

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+# add unique elements in list
+def uniq_add(my_list=[]):
+    sum = 0
+
+    if my_list == []:
+        return my_list
+
+    for num in set(my_list):
+        sum += num
+
+    return sum
