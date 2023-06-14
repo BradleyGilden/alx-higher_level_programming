76b34352f9d46ce9042eed8c19be35ed6147374c
BradleyGilden <bradleygilden@gmail.com>
2023-06-14 17:55:10 +0200

    delete dict key

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+
+# deletes entry in dictionary
+
+def simple_delete(a_dictionary, key=""):
+    if key in a_dictionary.keys():
+        a_dictionary.pop(key)
+        return a_dictionary
+    else:
+        return a_dictionary
