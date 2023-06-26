3e4384f097119e54ad3c94a1185f40e13694f907
BradleyGilden <bradleygilden@gmail.com>
2023-06-26 15:44:14 +0200

    magic calc

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+
+"""
+This function is the high level representation of bytecode
+"""
+
+
+def magic_calculation(a, b):
+    result = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            else:
+                result += a ** b / i
+        except Exception:
+            result = b + a
+            break
+    return (result)
