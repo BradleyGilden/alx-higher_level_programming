64e2455f668e53aa9501d1145218600d7545c02f
BradleyGilden <bradleygilden@gmail.com>
2023-06-26 12:39:55 +0200

    dividing lists safely

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python3
+
+"""
+safe_print_list - divides 2 lists element by element
+@my_list_1: divident list
+@my_list_2: divisor list
+@list_length: length of list
+Return: list of divided elements
+"""
+
+
+def list_division(my_list_1, my_list_2, list_length) -> list:
+    result = []
+    res = 0
+    i = 0
+    while i < list_length:
+        res = 0
+        try:
+            res = my_list_1[i] / my_list_2[i]
+        except ZeroDivisionError:
+            print("division by 0")
+        except (ValueError, TypeError):
+            print("wrong type")
+        except IndexError:
+            print("out of range")
+        finally:
+            result.append(res)
+            i += 1
+    return result
