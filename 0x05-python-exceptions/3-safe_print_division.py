5d14ccab5dca69f876da82f78cb7c25ebaf8a195
BradleyGilden <bradleygilden@gmail.com>
2023-06-26 11:39:13 +0200

    safe print division

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+
+"""
+check for zero division errors
+@a: divident
+@b: divisor
+"""
+
+
+def safe_print_division(a, b):
+    result = None
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        pass
+    finally:
+        print("Inside result: {}".format(result))
+        return result
