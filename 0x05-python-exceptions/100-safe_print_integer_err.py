a92363c09f419535d726e386418d5632e7ff5411
BradleyGilden <bradleygilden@gmail.com>
2023-06-26 13:28:42 +0200

    printing exception to stderr

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,3 +1,18 @@
 #!/usr/bin/python3
+from sys import stderr
+
+"""
+safe_print_list - prints value if value is an integer, if not print
+                  'Exception: ' followed by err message to stderr
+@value: integer given to print
+Return: True if int, False if not
+"""
+
 
 def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except (ValueError, TypeError) as e:
+        print(f"Exception: {e}", file=stderr)
+        return False
+    return True
