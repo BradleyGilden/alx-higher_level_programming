9b9b13e57db1ee8a4399a94f02d370d389fffa63
BradleyGilden <bradleygilden@gmail.com>
2023-06-26 15:38:09 +0200

    safe function

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+from sys import stderr
+
+"""
+safe_function - executes function safely
+@args: function arguments
+@fct: pointer to function
+Return: function return
+"""
+
+
+def safe_function(fct, *args):
+    ret = None
+
+    try:
+        ret = fct(args[0], args[1])
+    except Exception as e:
+        print(f"Exception: {e}", file=stderr)
+        return ret
+    return ret
