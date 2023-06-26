54514e8d1e109fa900320edd3149b97da76375e4
BradleyGilden <bradleygilden@gmail.com>
2023-06-26 10:55:19 +0200

    print safely using exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+
+"""
+safe_print_list - prints list, if index is greater then len(list) then print
+                  the whole list and return len(list)
+@mylist: list given to print
+@x: number of elements to print
+Return: returns number of elements to be printed
+"""
+
+
+def safe_print_list(my_list=[], x=0) -> int:
+    i = 0
+    while i < x:
+        try:
+            print(my_list[i], end="")
+        except IndexError:
+            print()
+            return i
+        i += 1
+    print()
+    return i
