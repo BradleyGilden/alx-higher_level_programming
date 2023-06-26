5daecda0aaa9fdcb462a5b4d2a03a55057adea63
BradleyGilden <bradleygilden@gmail.com>
2023-06-26 11:03:57 +0200

    print integers only

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+
+"""
+safe_print_list - prints value if value is an integer
+@mylist: integer given to print
+Return: True if int, False if not
+"""
+
+
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+    except ValueError:
+        return False
+    return True
