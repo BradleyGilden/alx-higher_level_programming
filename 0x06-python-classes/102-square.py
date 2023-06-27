b0fb027103799780dafed132620bb7557cd53dfa
BradleyGilden <bradleygilden@gmail.com>
2023-06-27 23:02:31 +0200

    compare objects

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+
+"""
+This module contains a Square class
+
+Author: Bradley Dillion Gilden
+Date: 27-06-2023
+"""
+
+
+class Square:
+    """Square initialises an instance attribute
+    Attributes(instance):
+        @private
+        size(int): stores the size of an object of Square
+    """
+
+    def __init__(self, size=0):
+        """__init__ - constructor
+        Args:
+            size(int): size of square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Returns current square area"""
+        return self.__size**2
+
+    @property
+    def size(self):
+        """size get method"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """size setter method
+        value(int): value to set"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def __eq__(self, other):
+        """compare obj1 == obj2"""
+        return self.size == other.size
+
+    def __ne__(self, other):
+        """compare obj1 != obj2"""
+        return self.size != other.size
+
+    def __le__(self, other):
+        """compare obj1 <= obj2"""
+        return self.size <= other.size
+
+    def __ge__(self, other):
+        """compare obj1 >= obj2"""
+        return self.size >= other.size
+
+    def __lt__(self, other):
+        """compare obj1 < obj2"""
+        return self.size < other.size
+
+    def __gt__(self, other):
+        """compare obj1 > obj2"""
+        return self.size > other.size
