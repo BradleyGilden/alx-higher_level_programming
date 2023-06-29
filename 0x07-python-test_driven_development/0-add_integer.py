c1888af2e1c2d77efd49eb7238782af5f88f818d
BradleyGilden <bradleygilden@gmail.com>
2023-06-29 20:36:18 +0200

    add integers

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """
 This module contains a function for adding integers
 
@@ -9,11 +8,19 @@ Date: 29-06-2023
 
 
 def add_integer(a, b=98):
-    """ returns the sum: a + b 
+    """returns the sum: a + b, as an integer
+
     Note: user must atleast assign a value to a
+          accepts integer and float arguments
+    Raises:
+        TypeError - if a or b is not of type int
     """
-    if not isinstance(a, int):
+    if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
-    if not isinstance(b, int):
+    if not isinstance(b, int) and not isinstance(a, float):
         raise TypeError("b must be an integer")
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
     return a + b
