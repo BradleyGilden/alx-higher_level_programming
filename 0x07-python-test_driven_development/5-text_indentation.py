4b300fb52c3549d2006bd39df3ae6b56fc83edf0
BradleyGilden <bradleygilden@gmail.com>
2023-06-30 16:43:32 +0200

    format text with specifiers .?:

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -6,3 +6,31 @@
 Author: Bradley Dillion Gilden
 Date: 29-06-2023
 """
+
+
+def text_indentation(text):
+    """prints 2 newlines after the characters: '.', '?' and ':'
+
+    Args:
+        text(str): text to be formatted
+
+    Raises:
+        TypeError: if {text} is not of type str
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    if text.strip(" ") == "":
+        return
+
+    temp = ""
+    start = 0
+    tlen = len(text)
+
+    for i in range(tlen):
+        if text[i] in ".?:" and start < tlen:
+            temp = text[start: i + 1].strip(" ")
+            start = i + 1
+            print(temp, end="\n\n")
+
+    if text[start:].strip(" ") != "":
+        print(text[start:].strip(" "), end="")
