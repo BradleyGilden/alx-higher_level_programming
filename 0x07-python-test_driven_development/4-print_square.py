779d6a07eb63f47724f48a236e4d9b18ededcdb2
BradleyGilden <bradleygilden@gmail.com>
2023-06-30 14:12:30 +0200

    prints square with #

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,8 +1,26 @@
 #!/usr/bin/python3
 
 """
-<module docstring>
+This module contains print_square which prints a square of #
 
 Author: Bradley Dillion Gilden
 Date: 29-06-2023
 """
+
+
+def print_square(size):
+    """prints a square of '#', of size: size * size
+
+    Args:
+        size: size of square
+
+    Raises:
+        TypeError: if size is not an integer
+        ValueError: if size < 0
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for _ in range(size):
+        print("#" * size)
