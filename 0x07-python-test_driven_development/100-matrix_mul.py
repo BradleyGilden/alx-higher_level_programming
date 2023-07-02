756f320bde350e7d881e036166f0e6c13caa548c
BradleyGilden <bradleygilden@gmail.com>
2023-07-02 13:27:27 +0200

    modified conditions

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -30,8 +30,8 @@ def matrix_mul(m_a, m_b):
         raise ValueError("m_a can't be empty")
     if not all(isinstance(row, list) for row in m_a):
         raise TypeError("m_a must be a list of lists")
-    if not (all((isinstance(item, int) or isinstance(item, float)))
-            for row in m_a for item in row):
+    if not (all((isinstance(item, int) or isinstance(item, float))
+            for row in m_a for item in row)):
         raise TypeError("m_a should contain only integers or floats")
     if not all(len(m_a[0]) == len(row) for row in m_a):
         raise TypeError("each row of m_a must be of the same size")
@@ -42,8 +42,8 @@ def matrix_mul(m_a, m_b):
         raise ValueError("m_b can't be empty")
     if not all(isinstance(row, list) for row in m_b):
         raise TypeError("m_b must be a list of lists")
-    if not (all((isinstance(item, int) or isinstance(item, float)))
-            for row in m_b for item in row):
+    if not (all((isinstance(item, int) or isinstance(item, float))
+            for row in m_b for item in row)):
         raise TypeError("m_b should contain only integers or floats")
     if not all(len(m_b[0]) == len(row) for row in m_b):
         raise TypeError("each row of m_b must be of the same size")
