3a76daec06ec5ec94657d4a9ae6721d7f9931c0e
BradleyGilden <bradleygilden@gmail.com>
2023-07-03 13:21:06 +0200

    empty class Rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+module contains an empty class: Rectangle
+
+Author: Bradley Dillion Gilden
+Date: 03-07-2023
+"""
+
+
+class Rectangle:
+    """Empty class {Rectangle} that does nothing"""
+    pass
