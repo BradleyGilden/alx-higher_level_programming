1025c6c758b65ce419c2fe8f5d4167b592daba8e
BradleyGilden <bradleygilden@gmail.com>
2023-07-03 15:23:08 +0200

    creating custon __str__ method

---
+++ 0x08-python-more_classes/main_tests/test_3-rectangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import sys
+
+
+sys.path.append("/home/nightlock/workspace/Python/alx-higher_level_programming/0x08-python-more_classes")
+Rectangle = __import__('3-rectangle').Rectangle
+
+my_rectangle = Rectangle(2, 4)
+print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
+
+print(str(my_rectangle))
+print(repr(my_rectangle))
+
+print("--")
+
+my_rectangle.width = 10
+my_rectangle.height = 3
+print(my_rectangle)
+print(repr(my_rectangle))
