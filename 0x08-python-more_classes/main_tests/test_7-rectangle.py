e658002ea3d4b2a58e36f7d77a56eb208b74707d
BradleyGilden <bradleygilden@gmail.com>
2023-07-03 18:02:52 +0200

    added public attribute symbol

---
+++ 0x08-python-more_classes/main_tests/test_7-rectangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+import sys
+
+
+sys.path.append("/home/nightlock/workspace/Python/alx-higher_level_programming/0x08-python-more_classes")
+Rectangle = __import__('7-rectangle').Rectangle
+
+my_rectangle_1 = Rectangle(8, 4)
+print(my_rectangle_1)
+print("--")
+my_rectangle_1.print_symbol = "&"
+print(my_rectangle_1)
+print("--")
+
+my_rectangle_2 = Rectangle(2, 1)
+print(my_rectangle_2)
+print("--")
+Rectangle.print_symbol = "C"
+print(my_rectangle_2)
+print("--")
+
+my_rectangle_3 = Rectangle(7, 3)
+print(my_rectangle_3)
+
+print("--")
+
+my_rectangle_3.print_symbol = ["C", "is", "fun!"]
+print(my_rectangle_3)
+
+print("--")
