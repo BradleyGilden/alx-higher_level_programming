c97a9bad7c1d19a9c82d60f344889e06c475a0ed
BradleyGilden <bradleygilden@gmail.com>
2023-07-03 15:04:35 +0200

    area and perimeter instance methods

---
+++ 0x08-python-more_classes/main_tests/test_2-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+
+
+sys.path.append("/home/nightlock/workspace/Python/alx-higher_level_programming/0x08-python-more_classes")
+Rectangle = __import__('2-rectangle').Rectangle
+
+my_rectangle = Rectangle(2, 4)
+print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
+
+print("--")
+
+my_rectangle.width = 10
+my_rectangle.height = 3
+print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
