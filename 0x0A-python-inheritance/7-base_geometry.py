0f37a6649aeb3241e0233027d88fe3fe638133ff
BradleyGilden <bradleygilden@gmail.com>
2023-07-10 18:46:31 +0200

    improved testing

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -29,6 +29,6 @@ class BaseGeometry:
             value(int): a non-zero positive integer value
         """
         if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
+            raise TypeError(f"{name} must be an integer")
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError(f"{name} must be greater than 0")
