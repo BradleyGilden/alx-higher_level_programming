7eb78c0f12bb226758ac951da07c4fd9cf71039d
BradleyGilden <bradleygilden@gmail.com>
2023-07-10 21:47:27 +0200

    updated doc

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -11,7 +11,9 @@ Date: 10-07-2023
 class MyInt(int):
     """MyInt inverts the == and != operators"""
     def __eq__(self, other):
+        """reverse of == in int parent class"""
         return not super().__eq__(other)
 
     def __ne__(self, other):
+        """reverse of != in int parent class"""
         return not super().__ne__(other)
