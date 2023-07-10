ac8bb092e492eaaaf011585e1b0d4e883f9fbf61
BradleyGilden <bradleygilden@gmail.com>
2023-07-10 19:28:36 +0200

    use base geometry as import

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -8,30 +8,7 @@ Date: 10-07-2023
 """
 
 
-class BaseGeometry:
-    """This class contains functions that raise exceptions
-
-    Raises:
-        Exception: when area() is called
-        TypeError: if value is not an integer
-        ValueError: if value <= 0
-    """
-
-    def area(self):
-        """raises an Exception always"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """validates if value is a correct type of integer
-
-        Args:
-            name(str): is always a string
-            value(int): a non-zero positive integer value
-        """
-        if type(value) != int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
@@ -46,6 +23,6 @@ class Rectangle(BaseGeometry):
         Rectangle object
         """
         self.integer_validator("width", width)
-        self.__width = width
         self.integer_validator("height", height)
+        self.__width = width
         self.__height = height
