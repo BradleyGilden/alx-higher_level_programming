6c55f5af49c578ffcc027838db51e0b2e52832c4
BradleyGilden <bradleygilden@gmail.com>
2023-07-10 18:42:44 +0200

    changed to format strings

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -29,6 +29,6 @@ class BaseGeometry:
             value(int): a non-zero positive integer value
         """
         if type(value) != int:
-            raise TypeError(f"{name} must be an integer")
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
