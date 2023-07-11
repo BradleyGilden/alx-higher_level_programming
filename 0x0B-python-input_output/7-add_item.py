52d55ba1fedad1bc98f0543cc1b5d20c468da6ab
BradleyGilden <bradleygilden@gmail.com>
2023-07-11 17:16:03 +0200

    formatted

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,8 +10,7 @@ from sys import argv
 
 
 if __name__ == '__main__':
-    save_to_json_file = \
-        __import__('5-save_to_json_file').save_to_json_file
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = \
         __import__('6-load_from_json_file').load_from_json_file
 
