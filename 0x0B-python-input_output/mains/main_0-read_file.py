47f80c68a8a0dc1028bfe7f8c3f9ccfe199b99eb
BradleyGilden <bradleygilden@gmail.com>
2023-07-11 11:25:11 +0200

    read_file testcase

---
+++ 0x0B-python-input_output/mains/main_0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import sys
+
+
+sys.path.append("/home/nightlock/workspace/Python/alx-higher_level_programming/0x0B-python-input_output")
+read_file = __import__('0-read_file').read_file
+
+read_file("text_files/my_file_0.txt")
