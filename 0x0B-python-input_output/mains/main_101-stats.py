eb95af57566cfccda363106c926d0e424ffbfb43
BradleyGilden <bradleygilden@gmail.com>
2023-07-11 22:21:03 +0200

    test compute metrics

---
+++ 0x0B-python-input_output/mains/main_101-stats.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import sys
+import random
+from time import sleep
+import datetime
+
+
+sys.path.append("/home/nightlock/workspace/Python/alx-higher_level_programming/0x0B-python-input_output")
+
+
+for i in range(10000):
+    sleep(random.random())
+    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
+        random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
+        datetime.datetime.now(),
+        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
+        random.randint(1, 1024)
+    ))
+    sys.stdout.flush()
