93f79b52b367af4f6b8c452919c4659812089d7d
BradleyGilden <bradleygilden@gmail.com>
2023-07-11 13:04:54 +0200

    object to json file

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+This module utlizes json module to implement to_json_string()
+
+Author: Bradley Dillion Gilden
+Date: 11-07-2023
+"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+
+    """writes object to file using JSON representation
+
+    Args;
+        my_obj(any): object to be converted
+        filename(str): file to be written to
+    """
+    with open(filename, "w", encoding="utf-8") as file:
+        json.dump(my_obj, file)
