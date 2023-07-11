4c2bd8ba48c6fd72a8a554a936929f057ece9be2
BradleyGilden <bradleygilden@gmail.com>
2023-07-11 18:54:20 +0200

    test improved to_json

---
+++ 0x0B-python-input_output/mains/main_10-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import sys
+
+
+sys.path.append("/home/nightlock/workspace/Python/alx-higher_level_programming/0x0B-python-input_output")
+Student = __import__('10-student').Student
+
+student_1 = Student("John", "Doe", 23)
+student_2 = Student("Bob", "Dylan", 27)
+
+j_student_1 = student_1.to_json()
+j_student_2 = student_2.to_json(['first_name', 'age'])
+j_student_3 = student_2.to_json(['middle_name', 'age'])
+
+print(j_student_1)
+print(j_student_2)
+print(j_student_3)
