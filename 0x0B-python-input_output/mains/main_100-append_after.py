ff934cdbbb2c2b7a1d04854c0e5bc379929d52fb
BradleyGilden <bradleygilden@gmail.com>
2023-07-11 22:08:30 +0200

    test append after

---
+++ 0x0B-python-input_output/mains/main_100-append_after.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+
+
+sys.path.append("/home/nightlock/workspace/Python/alx-higher_level_programming/0x0B-python-input_output")
+#!/usr/bin/python3
+append_after = __import__('100-append_after').append_after
+
+append_after("text_files/append_after_100.txt", "Python", "\"C is fun!\"\n")
