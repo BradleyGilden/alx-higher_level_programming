3840084f7ee204a23d7a2acfa196b1a49f81ef59
BradleyGilden <bradleygilden@gmail.com>
2023-07-15 15:07:58 +0200

    update from_json_string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -63,3 +63,14 @@ class Base:
             json_list = cls.to_json_string(expand_list)
             with open(filename, "w", encoding="utf-8") as file:
                 file.write(json_list)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """converts JSON string to a list of dictionaries
+
+        Args:
+        json_string(str): JSON string
+        """
+        if json_string is None or json_string == []:
+            return []
+        return json.loads(json_string)
