ca09fdb867b83403e62e9d0d0ebbee757950e2e5
BradleyGilden <bradleygilden@gmail.com>
2023-08-16 11:29:07 +0200

    create user with privileges

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- a script that creates the MySQL server user user_0d_1
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
+-- grant all privileges to user on the sever
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
