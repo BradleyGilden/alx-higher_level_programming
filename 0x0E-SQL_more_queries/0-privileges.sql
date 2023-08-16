2d1124dc9d53d1f3fd21b0182521a8ae1fa1c2e0
BradleyGilden <bradleygilden@gmail.com>
2023-08-16 13:57:12 +0200

    changed doc

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
--- a script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
+-- lists all privileges of the MySQL users user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- lists all privileges of the MySQL users user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
