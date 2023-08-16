70f4da485e60390b163127a5a932422909b6bf34
BradleyGilden <bradleygilden@gmail.com>
2023-08-16 14:27:30 +0200

    protected db creation from errors

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,10 +1,10 @@
 -- create a database hbtn_0d_usa
-CREATE DATABASE hbtn_0d_usa;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- set it as default schema
 USE hbtn_0d_usa;
 -- create table states with a primary key id that is auto-generated and
 -- a column name that can't be NULL
 CREATE TABLE IF NOT EXISTS states (
-	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	id INT PRIMARY KEY AUTO_INCREMENT,
 	name VARCHAR(256) NOT NULL
 );
