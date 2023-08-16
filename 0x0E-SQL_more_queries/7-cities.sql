6b7aaabc5f6c91f715a252690ed7154a3bdedcf2
BradleyGilden <bradleygilden@gmail.com>
2023-08-16 15:10:01 +0200

    added constraints

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,7 +3,7 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- create table cities with a primary key id that is uniquely auto-generated,
 -- name that can't be NULL and state_id that references the id of states
 CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
-	id INT PRIMARY KEY AUTO_INCREMENT UNIQUE,
+	id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
 	name VARCHAR(256) NOT NULL,
 	state_id INT NOT NULL,
 	FOREIGN KEY (`state_id`) REFERENCES `hbtn_0d_usa`.`states`(`id`)
