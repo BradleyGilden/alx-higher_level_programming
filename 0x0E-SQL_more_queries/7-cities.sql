3e7e57d833037cce8c1a921e16e3a55221d7ea08
BradleyGilden <bradleygilden@gmail.com>
2023-08-16 15:02:46 +0200

    made declarationse explicit

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,8 +1,10 @@
 -- create a database hbtn_0d_usa
-CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
-CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
-  `id` INT UNIQUE NOT NULL AUTO_INCREMENT,
-  `state_id` INT NOT NULL,
-  `name` VARCHAR(256) NOT NULL,
-  PRIMARY KEY (`id`),
-  FOREIGN KEY (`state_id`) REFERENCES `hbtn_0d_usa`.`states`(`id`));
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- create table cities with a primary key id that is uniquely auto-generated,
+-- name that can't be NULL and state_id that references the id of states
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+	id INT PRIMARY KEY AUTO_INCREMENT UNIQUE,
+	name VARCHAR(256) NOT NULL,
+	state_id INT NOT NULL,
+	FOREIGN KEY (`state_id`) REFERENCES hbtn_0d_usa.states(`id`)
+);
