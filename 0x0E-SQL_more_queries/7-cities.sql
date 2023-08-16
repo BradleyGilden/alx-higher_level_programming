667b550e639521b66547d9319d77c6ecfb0acd20
BradleyGilden <bradleygilden@gmail.com>
2023-08-16 15:16:40 +0200

    changed table creation formatting

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,9 +3,8 @@ CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 -- create table cities with a primary key id that is uniquely auto-generated,
 -- name that can't be NULL and state_id that references the id of states
 CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
-	`id` INT UNIQUE NOT NULL AUTO_INCREMENT,
-	`name` VARCHAR(256) NOT NULL,
-	`state_id` INT NOT NULL,
-	PRIMARY KEY (`id`),
-	FOREIGN KEY (`state_id`) REFERENCES `hbtn_0d_usa`.`states`(`id`)
-);
+  `id` INT UNIQUE NOT NULL AUTO_INCREMENT,
+  `state_id` INT NOT NULL,
+  `name` VARCHAR(256) NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`state_id`) REFERENCES `hbtn_0d_usa`.`states`(`id`));
