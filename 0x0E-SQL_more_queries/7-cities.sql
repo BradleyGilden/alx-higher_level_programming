c2d9b5f2f459147c59fbe14adec7906acb5733fb
BradleyGilden <bradleygilden@gmail.com>
2023-08-16 14:52:43 +0200

    added backquotes

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -8,5 +8,5 @@ CREATE TABLE IF NOT EXISTS cities (
 	id INT PRIMARY KEY AUTO_INCREMENT UNIQUE,
 	name VARCHAR(256) NOT NULL,
 	state_id INT NOT NULL,
-	FOREIGN KEY (state_id) REFERENCES states(id)
+	FOREIGN KEY (`state_id`) REFERENCES states(`id`)
 );
