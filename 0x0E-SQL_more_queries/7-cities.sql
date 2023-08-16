9aa1d2c77b359784e2b27486e00570df43470b2e
BradleyGilden <bradleygilden@gmail.com>
2023-08-16 15:04:41 +0200

    added backquotes

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,9 +2,9 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- create table cities with a primary key id that is uniquely auto-generated,
 -- name that can't be NULL and state_id that references the id of states
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
 	id INT PRIMARY KEY AUTO_INCREMENT UNIQUE,
 	name VARCHAR(256) NOT NULL,
 	state_id INT NOT NULL,
-	FOREIGN KEY (`state_id`) REFERENCES hbtn_0d_usa.states(`id`)
+	FOREIGN KEY (`state_id`) REFERENCES `hbtn_0d_usa`.`states`(`id`)
 );
