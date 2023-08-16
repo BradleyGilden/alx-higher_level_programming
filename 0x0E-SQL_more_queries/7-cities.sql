220ed7c9a5cb7fac32f4aaf7a57a689d43109247
BradleyGilden <bradleygilden@gmail.com>
2023-08-16 14:47:49 +0200

    cities table

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,12 @@
+-- create a database hbtn_0d_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- set it as default schema
+USE hbtn_0d_usa;
+-- create table cities with a primary key id that is uniquely auto-generated,
+-- name that can't be NULL and state_id that references the id of states
+CREATE TABLE IF NOT EXISTS cities (
+	id INT PRIMARY KEY AUTO_INCREMENT UNIQUE,
+	name VARCHAR(256) NOT NULL,
+	state_id INT NOT NULL,
+	FOREIGN KEY (state_id) REFERENCES states(id)
+);
