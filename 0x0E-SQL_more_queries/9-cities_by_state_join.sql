42b1bb44d1a9e7386b7c17e384d0b56ee2ebfc49
BradleyGilden <bradleygilden@gmail.com>
2023-08-16 19:40:50 +0200

    list cities by state

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,6 @@
+-- lists joint table of all the cities in the database hbtn_0d_usa
+SELECT cities.id, cities.name, states.name
+FROM cities
+INNER JOIN states
+ON cities.state_id = states.id
+ORDER BY cities.id ASC;
