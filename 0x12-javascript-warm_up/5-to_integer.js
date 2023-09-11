c76885f16275367bdb615b1b32886562c6ed56d9
BradleyGilden <bradleygilden@gmail.com>
2023-09-11 11:08:26 +0200

    style checked

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 /**
- * a script that prints My number: `<first argument converted in integer>` if 
+ * a script that prints My number: `<first argument converted in integer>` if
  * the first argument can be converted to an integer
  */
 
 const arg = process.argv[2];
 
 if (isNaN(arg)) {
-  console.log('Not a number')
+  console.log('Not a number');
 } else {
-  console.log('My number:', parseInt(arg))
+  console.log('My number:', parseInt(arg));
 }
