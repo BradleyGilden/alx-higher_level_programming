c5773508d7048af86cc153ea7c6ae3f64daa01f3
BradleyGilden <bradleygilden@gmail.com>
2023-09-11 10:45:06 +0200

    concatenation

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+/**
+ * a script that prints two arguments passed to it, in the following format:
+ * <arg1> is <arg2>
+ */
+const args = process.argv
+
+console.log(args[2] + ' is ' + args[3])
