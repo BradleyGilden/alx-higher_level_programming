54c7df8b4bbd9a41dd83088f24cdeb3462b0d290
BradleyGilden <bradleygilden@gmail.com>
2023-09-11 13:29:23 +0200

    second biggest number

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+
+/**
+ * a script that searches the second biggest integer in the list of arguments.
+ * You can assume all arguments can be converted to integer
+ * If no argument passed, print 0
+ * If the number of arguments is 1, print 0
+ */
+
+let args = process.argv;
+let arglen = args.length;
+
+if (arglen <= 3) {
+  console.log(0);
+} else {
+  args = args.slice(2).sort();
+  console.log(args[arglen - 4])
+}
