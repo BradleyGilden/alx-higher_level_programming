fdc29f5391798fce71fad95a51fd800c5b2732d2
BradleyGilden <bradleygilden@gmail.com>
2023-09-11 10:04:50 +0200

    processing arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+/**
+ * a script that prints a message depending of the number of arguments passed:
+ * If no arguments are passed to the script, print “No argument”
+ * If only one argument is passed to the script, print “Argument found”
+ * Otherwise, print “Arguments found”
+ */
+const arglen = process.argv.length;
+
+if (arglen <= 2) {
+  console.log('No argument');
+} else if (arglen === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
