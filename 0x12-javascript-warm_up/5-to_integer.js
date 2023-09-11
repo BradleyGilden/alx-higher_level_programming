86c33925790fa75182dd04d523dee4e9b2971937
BradleyGilden <bradleygilden@gmail.com>
2023-09-11 11:02:51 +0200

    checking for NaN

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+/**
+ * a script that prints My number: `<first argument converted in integer>` if 
+ * the first argument can be converted to an integer
+ */
+
+const num = parseInt(process.argv[2]);
+
+if (isNaN(num)) {
+  console.log('Not a number')
+} else {
+  console.log('My number:', num)
+}
