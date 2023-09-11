05ea5817f400927c8ef7f82774a2f0b1bb86bd80
BradleyGilden <bradleygilden@gmail.com>
2023-09-11 10:45:43 +0200

    added semi colons

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -3,6 +3,6 @@
  * a script that prints two arguments passed to it, in the following format:
  * <arg1> is <arg2>
  */
-const args = process.argv
+const args = process.argv;
 
-console.log(args[2] + ' is ' + args[3])
+console.log(args[2] + ' is ' + args[3]);
