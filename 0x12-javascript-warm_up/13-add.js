cdffa6ce01346ca4c997e544fe5a3c7c49a60837
BradleyGilden <bradleygilden@gmail.com>
2023-09-11 13:58:52 +0200

    export function

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+/**
+ * Write a function that returns the addition of 2 integers.
+ * The function must be visible from outside
+ * The name of the function must be add
+ */
+
+exports.add = function (a, b) {
+  return a + b;
+}
