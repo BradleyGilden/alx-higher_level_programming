4573ad9095fcc0c26b9dcdd00780142f697a3ea9
BradleyGilden <bradleygilden@gmail.com>
2023-09-11 12:22:29 +0200

    style checked

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,3 @@
 #!/usr/bin/node
 // a script that prints 3 lines
-console.log('C is fun')
-console.log('Python is cool')
-console.log('JavaScript is amazing')
+console.log('C is fun\nPython is cool\nJavaScript is amazing');
