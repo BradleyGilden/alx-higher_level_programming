104e64f0327556c341e3081d0e8f2c09705f8051
BradleyGilden <bradleygilden@gmail.com>
2023-09-11 12:43:24 +0200

    adding function

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+
+/**
+ * a script that prints the addition of 2 integers
+ * The first argument is the first integer
+ * The second argument is the second integer
+ * You have to define a function with this prototype: function add(a, b)
+ */
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
+
+console.log(add(a, b));
+
+function add (a, b) {
+  return a + b;
+}
