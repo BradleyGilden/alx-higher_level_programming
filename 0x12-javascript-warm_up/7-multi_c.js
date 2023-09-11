fce756412caebd962841f3b46aaca122b9b24435
BradleyGilden <bradleygilden@gmail.com>
2023-09-11 12:18:46 +0200

    using loops

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+
+/**
+ * a script that prints x times “C is fun”
+ * Where x is the first argument of the script
+ * If the first argument can’t be converted to an integer, print:
+ * “Missing number of occurrences”
+ */
+
+const x = process.argv[2];
+
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < parseInt(x); i++) {
+    console.log('C is fun');
+  }
+}
