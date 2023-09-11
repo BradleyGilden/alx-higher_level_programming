1099d34982c62be80d786415f890bbefa2c5806f
BradleyGilden <bradleygilden@gmail.com>
2023-09-11 12:36:18 +0200

    printing a square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+/**
+ * a script that prints a square
+ * The first argument is the size of the square
+ * If the first argument can’t be converted to an integer, print “Missing size”
+ * You must use the character X to print the square
+ * You must use console.log(...) to print all output
+ */
+
+const icon = 'X';
+let sides = process.argv[2];
+
+if (isNaN(sides)) {
+  console.log('Missing size');
+} else {
+  sides = parseInt(sides);
+  for (let i = 0; i < sides; i++) {
+    console.log(icon.repeat(sides));
+  }
+}
