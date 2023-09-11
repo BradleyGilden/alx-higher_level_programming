50d5b0b3d8a59f6ad6c5ec4e1eec49435b7a3bc8
BradleyGilden <bradleygilden@gmail.com>
2023-09-11 11:12:36 +0200

    print 3 lines using a loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+/**
+ * a script that prints 3 lines: (like 1-multi_languages.js) but by using:
+ * an array of string and a loop
+ */
+const messages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (const message of messages) {
+  console.log(message);
+}
