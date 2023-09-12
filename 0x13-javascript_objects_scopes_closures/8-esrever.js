ae96dc59a4339b77d30235bab959d10c19a943dc
BradleyGilden <bradleygilden@gmail.com>
2023-09-12 19:26:15 +0200

    reverse a list

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+
+// a function that returns the reversed version of a list
+exports.esrever = function (list) {
+  const len = list.length;
+  for (let i = 0, j = len - 1; i < Math.floor(len / 2); i++, j--) {
+    [list[i], list[j]] = [list[j], list[i]];
+  }
+  return list;
+};
