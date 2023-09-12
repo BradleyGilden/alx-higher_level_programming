4c1de5f3e16b462dcf8024d4fb71fc3778d284e2
BradleyGilden <bradleygilden@gmail.com>
2023-09-12 17:04:20 +0200

    base files

---
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+
+// an empty class Rectangle that defines a rectangle
+module.exports = class Rectangle {};
---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+/**
+ * a class Rectangle that defines a rectangle:
+ * You must use the class notation for defining your class
+ * The constructor must take 2 arguments w and h
+ * Initialize the instance attribute width with the value of w
+ * Initialize the instance attribute height with the value of h
+ */
+
+module.exports = class Rectangle {
+  width;
+  height;
+
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+};
