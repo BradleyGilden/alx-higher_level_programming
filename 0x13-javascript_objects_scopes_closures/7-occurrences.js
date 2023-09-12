059a62e0c0d001e1c5293755caa263b75928ca14
BradleyGilden <bradleygilden@gmail.com>
2023-09-12 19:11:28 +0200

    count occurences

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+
+exports.nbOccurences = function (list, searchElement) {
+  let occurences = 0;
+  for (const items of list) {
+    occurences = items === searchElement ? occurences + 1 : occurences;
+  }
+  return occurences;
+};
