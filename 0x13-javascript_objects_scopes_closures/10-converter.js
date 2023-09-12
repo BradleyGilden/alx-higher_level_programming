841d2471b1d7e0283f1af4b015bfb8a664f4bb76
BradleyGilden <bradleygilden@gmail.com>
2023-09-12 19:56:45 +0200

    closure

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+
+// a function that converts a number from base 10 to another base
+exports.converter = function (base) {
+  return function (num) {
+    return num.toString(base);
+  };
+};
