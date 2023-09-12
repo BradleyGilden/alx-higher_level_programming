998bf6678019f834fe7b901021fbebea95e55af7
BradleyGilden <bradleygilden@gmail.com>
2023-09-12 20:00:20 +0200

    closure

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 /**
- * a function that prints the number of arguments already printed and
+ * function that prints the number of arguments already printed and
  * the new argument value
  */
 let count = 0;
