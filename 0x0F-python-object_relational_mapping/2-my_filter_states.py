6a5386da12bbbce54999a329a1ed21ddb2dac2f7
BradleyGilden <bradleygilden@gmail.com>
2023-09-14 09:11:57 +0200

    filter list

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python3
 
 """
-<module docstring>
+This module utilizes MySQLdb to execute queries
 
 Author: Bradley Dillion Gilden
 Date: 14-09-2023
 """
+import MySQLdb
+from sys import argv
+
+
+if __name__ == '__main__':
+    conn = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM states WHERE name = '{}';".format(argv[4]))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    conn.close()
