3c2dfc4baf1421a1123ff76ec7908457c40db167
BradleyGilden <bradleygilden@gmail.com>
2023-09-14 16:24:00 +0200

    update orm setup

--- 0x0F-python-object_relational_mapping/db_setup/ormsetup.sql
+++ 0x0F-python-object_relational_mapping/db_setup/ormsetup.sql
@@ -1,3 +1,5 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0e_6_usa;
 USE hbtn_0e_6_usa;
 SHOW CREATE TABLE states;
+-- Insert states
+INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
