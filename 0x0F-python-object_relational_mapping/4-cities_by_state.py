d6fe9e815d1dcf16b4ff87c40f09a9887b949101
BradleyGilden <bradleygilden@gmail.com>
2023-09-14 12:43:02 +0200

    added semi-colon

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     cursor = conn.cursor()
     cursor.execute("SELECT cities.id, cities.name, states.name FROM cities \
                    INNER JOIN states \
-                   ON cities.state_id = states.id")
+                   ON cities.state_id = states.id;")
     rows = cursor.fetchall()
     for row in rows:
         print(row)
