1a8fe1e6aba04a54a86a640de6daf0bc8944f8c5
BradleyGilden <bradleygilden@gmail.com>
2023-09-14 20:13:22 +0200

    updating a state

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,8 +1,26 @@
 #!/usr/bin/python3
 
 """
-<module docstring>
+a script that lists all State objects from the database hbtn_0e_6_usa
 
 Author: Bradley Dillion Gilden
 Date: 14-09-2023
 """
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter_by(id=2).one()
+    state.name = 'New Mexico'
+    session.commit()
+    session.close()
