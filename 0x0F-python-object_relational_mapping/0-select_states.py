32d69488dceb0b3bfa2209ba73d89a66845d6d3a
BradleyGilden <bradleygilden@gmail.com>
2023-09-14 08:51:17 +0200

    list table items

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+This module utilizes MySQLdb to execute queries
+
+Author: Bradley Dillion Gilden
+Date: 14-09-2023
+"""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == '__main__':
+    conn = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM states;')
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    conn.close()
