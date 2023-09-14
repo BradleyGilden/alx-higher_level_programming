8f3671db84f743d8b5637cc355f480d239fb5b68
BradleyGilden <bradleygilden@gmail.com>
2023-09-14 16:15:21 +0200

    add base to import

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -10,7 +10,7 @@ Date: 14-09-2023
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
+from model_state import Base, State
 
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
