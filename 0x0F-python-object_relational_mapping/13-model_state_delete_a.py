553f6298da5a6ab6f4a5db3d6ba819c4a03d69d7
BradleyGilden <bradleygilden@gmail.com>
2023-09-14 21:27:23 +0200

    delete entries

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,8 +1,29 @@
 #!/usr/bin/python3
 
 """
-<module docstring>
+a script that deletes all State objects with a name containing the letter a
+from the database hbtn_0e_6_usa
 
 Author: Bradley Dillion Gilden
 Date: 14-09-2023
 """
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State)
+    for state in states:
+        if 'a' in state.name:
+            session.delete(state)
+    session.commit()
+    session.close()
