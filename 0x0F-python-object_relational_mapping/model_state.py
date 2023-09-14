2b1da89ff3807f15107957d2ad060361b9d2e7a5
BradleyGilden <bradleygilden@gmail.com>
2023-09-14 15:52:05 +0200

    create state model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,8 +1,28 @@
 #!/usr/bin/python3
 
 """
-<module docstring>
+This module defines a State class for states table
 
 Author: Bradley Dillion Gilden
 Date: 14-09-2023
 """
+
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    """This class is blueprint for the states table
+
+    __tablename__ (str): the name of the table
+    id (sqlalchemy.Integer): The state's id.
+    name (sqlalchemy.String): The state's name.
+    """
+    __tablename__ = "states"
+
+    id = Column('id', Integer, primary_key=True, nullable=False,
+                autoincrement=True)
+    name = Column('name', String(length=128), nullable=False)
