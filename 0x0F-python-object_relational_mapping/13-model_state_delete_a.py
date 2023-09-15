b6cc7059e1e6b1d7c969b1809d19780b617ffd4d
BradleyGilden <bradleygilden@gmail.com>
2023-09-15 17:48:25 +0200

    shortened query

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -21,7 +21,6 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for state in session.query(State):
-        if "a" in state.name:
-            session.delete(state)
+    for state in session.query(State).filter(State.name.like("%a%")):
+        session.delete(state)
     session.commit()
