5500adc2189c2b9c45b93c2e17948b9e126834c9
BradleyGilden <bradleygilden@gmail.com>
2023-09-15 10:43:54 +0200

    added case sensitivity

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,9 +11,10 @@ from sys import argv
 
 
 if __name__ == '__main__':
+    query = "SELECT * FROM states WHERE BINARY name = '{}';"
     conn = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM states WHERE name = '{}';".format(argv[4]))
+    cursor.execute(query.format(argv[4]))
     rows = cursor.fetchall()
     for row in rows:
         print(row)
