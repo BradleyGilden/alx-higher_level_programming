988c17ef8b966940170b6ba1cc554ac75b03c04b
BradleyGilden <bradleygilden@gmail.com>
2023-09-28 16:15:06 +0200

    remove echo

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 # get content length of get request
-
-length=$(curl -sI "$1" | grep -oP '(?<=Content-Length:\s)\d+')
-echo "$length"
+curl -sI "$1" | grep -oP '(?<=Content-Length:\s)\d'
