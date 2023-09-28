b849f0d5a7564b287e93f2d4a381e9321f504f8a
BradleyGilden <bradleygilden@gmail.com>
2023-09-28 22:10:49 +0200

    included silent curling

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # JSON POST request to a URL passed as the first argument
-curl -d "@${2}" -H "Content-Type: application/json" -X POST "$1"
+curl -sd "@${2}" -H "Content-Type: application/json" -X POST "$1"
