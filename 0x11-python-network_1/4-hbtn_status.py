f7c34d4a2a5e2d53d9e335cc1c5d86b726d82f13
BradleyGilden <bradleygilden@gmail.com>
2023-09-29 13:12:04 +0200

    added tabs

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -12,5 +12,5 @@ import requests
 
 if __name__ == '__main__':
     response = requests.get('https://alx-intranet.hbtn.io/status')
-    print("Body response:", f"type: {type(response.text)}",
-          f"content: {response.text}", sep='\n')
+    print("Body response:", f"\t- type: {type(response.text)}",
+          f"\t- content: {response.text}", sep='\n')
