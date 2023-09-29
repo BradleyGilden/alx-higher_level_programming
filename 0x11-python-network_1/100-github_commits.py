b8f963d37b3365a6c39deec379632f774e14ddad
BradleyGilden <bradleygilden@gmail.com>
2023-09-29 16:17:06 +0200

    advanced task

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""
+Please list 10 commits (from the most recent to oldest) of the repository
+“rails” by the user “rails”
+You must use the GitHub API, here is the documentation
+https://developer.github.com/v3/repos/commits/
+Print all commits by: `<sha>: <author name>` (one by line)
+
+Author: Bradley Dillion Gilden
+Date: 29-09-2023
+"""
+import sys
+import requests
+
+
+if __name__ == '__main__':
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+    headers = {
+        "X-GitHub-Api-Version": "2022-11-28",
+        "Accept": "application/vnd.github+json"
+    }
+
+    response = requests.get(
+        f'https://api.github.com/repos/{owner}/{repo}/commits', headers=headers
+        )
+    for user in response.json():
+        user_name = user.get('commit').get('author').get('name')
+        print(f"{user.get('sha')}: {user_name}")
--- 0x11-python-network_1/README.md
+++ 0x11-python-network_1/README.md
@@ -43,3 +43,6 @@ followed by the value of the HTTP status code
   * The first argument will be your username
   * The second argument will be your password (in your case, a personal access token as password)
   * You must use the package requests and sys
+* [100-github_commits.py](100-github_commits.py) - Please list 10 commits (from the most recent to oldest) of the repository “rails” by the user “rails”.
+You must use the GitHub API, here is the documentation [https://developer.github.com/v3/repos/commits/](https://developer.github.com/v3/repos/commits/).
+Print all commits by: `<sha>: <author name>` (one by line)
