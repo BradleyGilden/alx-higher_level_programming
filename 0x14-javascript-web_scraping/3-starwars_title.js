11295cc5d3ca2afe628ef82c23526773dee17199
BradleyGilden <bradleygilden@gmail.com>
2023-10-24 13:46:57 +0200

    update doc

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
 
 /**
- * a script that prints the title of a Star Wars movie where the episode number matches a given integer.
+ * a script that prints the title of a Star Wars movie where the episode number
+ * matches a given integer.
  *    The first argument is the movie ID
- *    You must use the Star wars API with the endpoint https://swapi-api.alx-tools.com/api/films/:id
+ *    You must use the Star wars API with the endpoint
+ *        https://swapi-api.alx-tools.com/api/films/:id
  *    You must use the module request
  *
  * Author: Bradley Dillion Gilden
