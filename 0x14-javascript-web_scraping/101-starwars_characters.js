3105f4a11e07b000700eeaff0079c030cd525ecb
BradleyGilden <bradleygilden@gmail.com>
2023-10-24 16:45:45 +0200

    list characters as they are ordered

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+/**
+ * prints characters in a specified movie in the order they are listed
+ * Author: Bradley Dillion Gilden
+ * Date: 24-10-2023
+ */
+
+const request = require('request');
+const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
+request(url, function (error, response, body) {
+  if (!error) {
+    let characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
+  }
+});
+
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
