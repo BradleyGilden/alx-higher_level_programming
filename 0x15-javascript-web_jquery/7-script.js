4b5627b68806962847534bfbd197921260b3e6bc
BradleyGilden <bradleygilden@gmail.com>
2023-10-30 21:33:58 +0200

    ajax request

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,9 @@
+$.ajax({
+  url: 'https://swapi-api.hbtn.io/api/people/5/?format=json', // URL to send the request
+  method: 'GET', // HTTP method (GET, POST, etc.)
+  dataType: 'json', // Expected data type
+  success: (data) => {
+    // This function is executed if the request is successful
+    $('DIV#character').text(data.name)
+  }
+});
