90bfe648f9f8c708f6c357d40b6e3f32e11b22c5
BradleyGilden <bradleygilden@gmail.com>
2023-10-30 22:25:02 +0200

    made executable

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,4 +1,4 @@
-$.get('https://swapi-api.hbtn.io/api/films/?format=json', (data)=>{
+$.get('https://swapi-api.hbtn.io/api/films/?format=json', (data) => {
   $('UL#list_movies')
-  .append(...data.results.map(movie => `<li>${movie.title}</li>`));
+    .append(...data.results.map(movie => `<li>${movie.title}</li>`));
 });
